543cb8cc441a6d2883028067ec4280133639db0e
Alexander Korling <lermannen@gmail.com>
2014-05-13 08:51:35 +0200

    Databasanslutningen görs via miljövariabel.

--- model.rb
+++ model.rb
@@ -1,6 +1,6 @@
 require 'sequel'
 
-DB = Sequel.connect('postgres://akorling@localhost:5432/movieingon')
+DB = Sequel.connect(ENV['DATABASE_URL'])
 
 class Person < Sequel::Model(:persons)
   many_to_many :movies, :class => :Movie, :right_key => :movie_id, :join_table => :crew, :select=>[Sequel.expr(:movies).*, :crew__job]
